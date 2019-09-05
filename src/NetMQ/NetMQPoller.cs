38be63bea105eba4bf3ca294c1924a2a0cc72028
longfin <longfinfunnel@gmail.com>
2019-09-05 22:30:02 +0900

    Allow NetMQPoller.QueueTask() even if disposed. (no-op)

--- src/NetMQ/NetMQPoller.cs
+++ src/NetMQ/NetMQPoller.cs
@@ -104,13 +104,9 @@ namespace NetMQ
         {
             if (task == null)
                 throw new ArgumentNullException(nameof(task));
-            CheckDisposed();
 
-            // We are not allowing new tasks will disposing
-            if (m_disposeState == (int)DisposeState.Disposing)
-                throw new ObjectDisposedException("NetMQPoller");
-
-            m_tasksQueue.Enqueue(task);
+            if (m_disposeState == (int)DisposeState.Undisposed)
+                m_tasksQueue.Enqueue(task);
         }
 
         public void Run([NotNull] Action action)
